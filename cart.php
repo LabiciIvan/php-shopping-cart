8103d9b2317a7588339cbee4035122fd8f9a10d5
Labici Ivan <labici.ioan@yaoo.com>
2023-12-02 22:32:19 +0200

    PSC-1: Refactor initial commit and implement cart.
    - refactored initial code and files names;
    
    - added the cart view to diplay each product from the cart;

--- cart.php
+++ cart.php
@@ -1,5 +1,41 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Shopping Cart</title>
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<?php require "./partials/navbar.php" ?>
+	<?php if (isset($cookie_products)) { ?>
+		<table class="cart-table">
+			<tr>
+				<th>Name</th>
+				<th>Quantity</th>
+				<th>Price</th>
+				<th>Increase/Decrease</th>
+				<th>Delete</th>
+			</tr>
+	<?php } ?>
 
-$cookie_products = (isset($_COOKIE['products']) ? json_decode($_COOKIE['products'], true) : null);
+	<?php
+	if (isset($cookie_products)) {
+		foreach ($cookie_products['products'] as $key => $product) {
+			echo "<tr>" .
+					"<td>{$product['name']}</td>" .
+					"<td>{$product['quantity']}</td>" .
+					"<td>{$product['price']}</td>" .
+					"<td><button class='add-btn'>+</button><button class='add-btn'>-</button></td>" .
+					"<td><button class='cart-link' onClick=\"callToApi('".$product['id']."', 'remove')\">Remove</button></td>" .
+				"</tr>";
+		}
+	} else {
+		echo "<div> No items in cart !</div>";
+	}
+	?>
+	</table>
 
-?>
+	<script src="./requestsAPI.js" defer></script>
+</body>
+</html>
--- products.php
+++ products.php
@@ -7,21 +7,25 @@ $data = [
 	],
 	'products' => [
 		[
+			'id'	=> 0,
 			'image'	=> './images/pencils.png',
 			'name'	=> 'Pencil',
 			'price'	=> '7',
 		],
 		[
+			'id'	=> 1,
 			'image'	=> './images/eraser.png',
 			'name'	=> 'Eraser',
 			'price'	=> '5',
 		],
 		[
+			'id'	=> 2,
 			'image'	=> './images/acrylics.png',
 			'name'	=> 'Acrylics Paint',
 			'price'	=> '19.99',
 		],
 		[
+			'id'	=> 3,
 			'image'	=> './images/brushes.png',
 			'name'	=> 'Paint Brush',
 			'price'	=> '20',
